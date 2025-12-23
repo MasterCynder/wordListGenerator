4564aa707d2a7c08433a91ea7fbca529c5980ed8
MasterCynder <MasterCynder@hotmail.com>
2025-12-23 16:48:02 +0100

    Replacing getopt (old C style) with argparse, which is the current Python standard for handling command-line arguments elegantly.

--- wordListGenerator.py
+++ wordListGenerator.py
@@ -1,131 +1,84 @@
 import sys
-import getopt
-import lib
+import argparse
 from lib import word as wd
 from lib import open_file as of
 from lib import write_file as wf
-from lib import usage as us
-from lib import combination as co
-from lib import permutation as pm
 from lib import separator as sp
 from lib import progress_bar as pb
 
-def main(argv):
+def main():
+    parser = argparse.ArgumentParser(description="Word list generator v0.2")
+    parser.add_argument("-w", "--write", help="Path for writing the word list")
+    parser.add_argument("-k", "--keywords", required=True, help="Path for reading keywords")
+    parser.add_argument("-l", "--leet", action="store_true", help="Using leet conversion")
+    parser.add_argument("-C", "--uppercase", action="store_true", help="Using uppercase")
+    parser.add_argument("-c", "--lowercase", action="store_true", help="Using lowercase")
+    parser.add_argument("-e", "--camel", action="store_true", help="Using camelcase")
+    parser.add_argument("-o", "--optional", action="store_true", help="Make each word optional")
+    parser.add_argument("-d", "--disorder", action="store_true", help="All possible word orders")
+    parser.add_argument("-a", "--startpunctuation", action="store_true", help="Add a punctuation mark at the beginning")
+    parser.add_argument("-z", "--endpunctuation", action="store_true", help="Add a punctuation mark at the end")
+    parser.add_argument("-m", "--middlepunctuation", action="store_true", help="Add a punctuation mark between the words")
+    parser.add_argument("-s", "--simulation", action="store_true", help="Simulation only")
 
-    writeFilePath = ''
-    keyWordsFilePath = ''
-    leet = False
-    lowercase = False
-    uppercase = False
-    optional = False
-    disorder = False
-    startChar = False;
-    endedChar = False;
-    separatorChar = False;
-    simulation = False
-    camelcase = False
+    args = parser.parse_args()
 
-    try:                                
-        opts, args = getopt.getopt(argv, "hw:k:lCceodazms", ["help", "write=", "keywords=", "leet", "uppercase", "lowercase", "camel", "optional", "disorder", "startpunctuation", "endpunctuation", "middlepunctuation", "simulation"])
-    except getopt.GetoptError:          
-        us.usage()
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            us.usage()
-            sys.exit()
-        elif opt in ("-w", "--write"):
-            writeFilePath = arg
-        elif opt in ("-k", "--keywords"):
-            keyWordsFilePath = arg
-        elif opt in ("-l", "--leet"):
-            leet = True
-        elif opt in ("-c", "--lowercase"):
-            lowercase = True
-        elif opt in ("-e", "--camel"):
-            camelcase = True
-        elif opt in ("-C", "--uppercase"):
-            uppercase = True
-        elif opt in ("-o", "--optional"):
-            optional = True
-        elif opt in ("-d", "--disorder"):
-            disorder = True
-        elif opt in ("-a", "--startpunctuation"):
-            startChar = True
-        elif opt in ("-z", "--endpunctuation"):
-            endedChar = True
-        elif opt in ("-m", "--middlepunctuation"):
-            separatorChar = True
-        elif opt in ("-s", "--simulation"):
-            simulation = True
-
-    if writeFilePath == '' and simulation == False:
+    if not args.write and not args.simulation:
         print("Error: -w/--write or -s/--simulation option required")
-        us.usage()
-        sys.exit(2)        
-    if keyWordsFilePath == '':
-        print("Error: -k/--keywords option required")
-        us.usage()
         sys.exit(2)
 
-    keyWordFile = of.OpenFile(keyWordsFilePath)
+    # Chargement des mots
+    keyWordFile = of.OpenFile(args.keywords)
     keyWordFile.read()
     keyWordFile.loadKeyWord()
     keyWordList = keyWordFile.returnKeyWord()
+    
     globalTab = []
-
-    weight = 1
-    tmpWeight = 0
     for possibilities in keyWordList:
         wordsTab = []
         for string in possibilities:
-            myWord = wd.Word(string)
-            if (leet == True):
-                myWord.addLeet()
-            if (lowercase == True):
-                myWord.addLowerCase()
-            if (uppercase == True):
-                myWord.addUpperCase()
-            if (camelcase == True):
-                myWord.addCamelCase()
-            if (optional == True):
-                myWord.addOptionalWord()
+            if not string.strip(): continue # Skip empty lines
+            myWord = wd.Word(string.strip())
+            if args.leet: myWord.addLeet()
+            if args.lowercase: myWord.addLowerCase()
+            if args.uppercase: myWord.addUpperCase()
+            if args.camel: myWord.addCamelCase()
+            if args.optional: myWord.addOptionalWord()
             myWord.loadNumbers()
-            tmpWeight = tmpWeight + myWord.weightPossibilities()
             wordsTab.append(myWord)
-        weight = weight * tmpWeight
-        globalTab.append(wordsTab)
+        if wordsTab: globalTab.append(wordsTab)
     
     mySeparator = sp.Separator(globalTab)
-    if disorder == True:
-        mySeparator.addPermutation()
-    if startChar == True:
-        mySeparator.addCharStarted()
-    if endedChar == True:
-        mySeparator.addCharEnded()
-    if separatorChar == True:
-        mySeparator.addCharSeparated()
+    if args.disorder: mySeparator.addPermutation()
+    if args.startpunctuation: mySeparator.addCharStarted()
+    if args.endpunctuation: mySeparator.addCharEnded()
+    if args.middlepunctuation: mySeparator.addCharSeparated()
+    
     mySeparator.loadNumbers()
     nb = mySeparator.returnNbCombination()
-    if simulation == False:
-        ProgressBar = None
-        percent = 0
-        file = wf.WriteFile(writeFilePath)
-        for i in range(nb):
-            file.write(mySeparator.convertNumberInCombination(i)+"\n")
-            label = "Loading " + "(" + str(i) + "/" + str(nb) + ")"
-            if ProgressBar == None:
-                ProgressBar = pb.ProgressBar(i, nb, label = label, usePercentage = True)
-            elif ((i / nb * 100) >= (percent + 1) or i == (nb - 1)):
-                ProgressBar.updateProgress(i+1, label)
-                percent = percent + 1
-        print()
-        file.close()
-    if simulation == True:
-        print(str(nb) + " Combinations")
-        # print(str(weight / 1024) + " Ko")
+
+    if args.simulation:
+        print(f"{nb} Combinations possibles.")
+        return
+
+    # Ecriture avec barre de progression optimisée
+    file = wf.WriteFile(args.write)
+    pbar = None
+    
+    # On n'update la barre que tous les 1% pour gagner en performance
+    step = max(1, nb // 100)
+    
+    for i in range(nb):
+        file.write(mySeparator.convertNumberInCombination(i) + "\n")
+        if i % step == 0 or i == nb - 1:
+            label = f"Generating ({i}/{nb})"
+            if pbar is None:
+                pbar = pb.ProgressBar(i, nb, label=label)
+            else:
+                pbar.updateProgress(i + 1, label)
+    
+    file.close()
+    print("\nTerminé !")
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
-
+    main()
